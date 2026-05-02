4392d7205077af71edd6b8a1eb76e789ae459d5a
tpapaseny-netizen <ton@email.com>
2026-05-02 20:24:20 +0000

    Fix Aida - popup au lieu d'iframe

--- add_aida_widget.py
+++ add_aida_widget.py
@@ -5,21 +5,14 @@ with open('index.html', 'r', encoding='utf-8') as f:
 with open('aida-widget.html', 'r', encoding='utf-8') as f:
     widget = f.read()
 
-# Remove old widget if exists
-if 'aida-fab' in c:
-    print("Widget already exists - updating...")
-    import re
-    c = re.sub(r'<!-- AIDA FLOATING.*?<!-- FIN AIDA FLOATING CHAT -->', '', c, flags=re.DOTALL)
+import re
+# Remove old widget
+c = re.sub(r'<!-- AIDA FLOATING.*?<!-- FIN AIDA FLOATING CHAT -->', '', c, flags=re.DOTALL)
 
 # Insert before </body>
-if '</body>' in c:
-    c = c.replace('</body>', widget + '</body>', 1)
-    print("Widget added before </body>")
-else:
-    c += widget
-    print("Widget appended at end")
+c = c.replace('</body>', widget + '</body>', 1)
 
 with open('index.html', 'w', encoding='utf-8') as f:
     f.write(c)
-
-print("Done! aida-fab in file:", 'aida-fab' in c)
+print("Done! Widget updated in index.html")
+print("aida-fab in file:", 'aida-fab' in c)
