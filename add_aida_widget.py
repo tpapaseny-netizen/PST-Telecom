8d766348ad333b83cbd95a79f5146cb23dbedb67
tpapaseny-netizen <ton@email.com>
2026-05-02 23:14:13 +0000

    Aida - suppression tous les anciens widgets

--- add_aida_widget.py
+++ add_aida_widget.py
@@ -1,18 +1,27 @@
 
+import re
+
 with open('index.html', 'r', encoding='utf-8') as f:
     c = f.read()
 
+before = len(c)
+
+# Remove ALL previous aida widget versions
+c = re.sub(r'<!-- AIDA CHAT PST -->.*?<!-- FIN AIDA CHAT PST -->', '', c, flags=re.DOTALL)
+c = re.sub(r'<!-- AIDA CHAT INTEGRE -->.*?<!-- FIN AIDA CHAT INTEGRE -->', '', c, flags=re.DOTALL)
+c = re.sub(r'<!-- AIDA FLOATING CHAT BUTTON -->.*?<!-- FIN AIDA FLOATING CHAT -->', '', c, flags=re.DOTALL)
+c = re.sub(r'<!-- AIDA FLOATING CHAT -->.*?<!-- FIN AIDA FLOATING CHAT -->', '', c, flags=re.DOTALL)
+
+# Add new widget before </body>
 with open('aida-widget.html', 'r', encoding='utf-8') as f:
     widget = f.read()
 
-import re
-# Remove old widget
-c = re.sub(r'<!-- AIDA FLOATING.*?<!-- FIN AIDA FLOATING CHAT -->', '', c, flags=re.DOTALL)
-
-# Insert before </body>
 c = c.replace('</body>', widget + '</body>', 1)
 
 with open('index.html', 'w', encoding='utf-8') as f:
     f.write(c)
-print("Done! Widget updated in index.html")
-print("aida-fab in file:", 'aida-fab' in c)
+
+print('Before:', before, 'After:', len(c))
+print('aida-tip in file:', 'aida-tip' in c)
+print('aida-fab in file:', 'aida-fab' in c)
+print('Tooltip count:', c.count('aida-tip'))
